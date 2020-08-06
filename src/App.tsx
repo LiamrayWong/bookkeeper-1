7c2f3b19cf2d2530984649a2d7f528f69aeb810b
Ryan <1079388320@qq.com>
2020-08-06 16:34:15 +0800

    add Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
-
+import Nav from './components/Nav';
 const Wrapper = styled.div`
   height: 100vh;
   display: flex;
@@ -17,17 +16,7 @@ const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.nav`
-    border: 1px solid green;
-    > ul{
-      display: flex;
-      > li {
-        width: 33.3333%;
-        text-align: center;
-        padding: 16px;
-      }
-    }
-`;
+
 
 function App() {
   return (
@@ -50,19 +39,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
