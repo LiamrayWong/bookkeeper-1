a855b421df9f3c92d9ff1862bbd222fe82f27707
Ryan <1079388320@qq.com>
2020-08-07 12:23:42 +0800

    create views directory

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,32 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import Layout from './components/Layout';
-
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 function App() {
   return (
     <Router>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-function NoMatch() {
-  return <div>页面不存在，地址错误</div>;
-}
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页</h2>
-    </Layout>
-  );
-}
 
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页</h2>
-    </Layout>
-  );
-}
 
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页</h2>
-    </Layout>
-  );
-}
 export default App;
