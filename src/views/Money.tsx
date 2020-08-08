1fec9c9c22684bb6643fff2fe778ef7a755d5609
Ryan <1079388320@qq.com>
2020-08-08 13:23:00 +0800

    complete UI of CategorySection in Money.tsx

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -49,6 +49,27 @@ const NotesSection = styled.section`
   }
 `;
 const CategorySection = styled.section`
+  font-size: 24px;
+  > ul{
+    display: flex;
+    background: #c4c4c4;
+    > li{
+     padding: 18px 0;
+     width: 50%;
+     text-align: center;
+     position: relative;
+     &.selected::after{
+      content: "";
+      display: block;
+      height: 3px;
+      background: #333;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      left: 0;
+     }
+    }
+  }  
 `;
 
 const NumberPad = styled.section`
@@ -76,7 +97,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
