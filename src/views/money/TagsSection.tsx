da39710bf6b1444e682ff1acd4db60358fbe41a9
Ryan <1079388320@qq.com>
2020-08-12 13:38:21 +0800

    createId.tsx

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
+import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
   background: #FFFFFF;
@@ -46,7 +47,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('请输入新的标签');
     if (tagName !== null) {
-      setTags([...tags, {id:Math.random(),name:tagName}]);
+      setTags([...tags, {id:createId(),name:tagName}]);
     }
   };
   const onToggleTag = (tagId: number) => {
