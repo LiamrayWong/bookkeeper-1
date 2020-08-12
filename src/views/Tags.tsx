df8f514cab3726992db62a2c4fa3aba175b109e7
Ryan <1079388320@qq.com>
2020-08-12 10:46:37 +0800

    create route path:'/tags/:tag' add exact property

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   background: white;
   font-size: 16px;
   > li{
     border-bottom: 1px solid #d5d5d5;
-    padding: 12px 0 12px 0;
-    margin: 0 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    margin: 0 16px; 
+    > a{
+      padding: 12px 0 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
   
 `;
@@ -32,10 +35,10 @@ const Center = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`
-const Space= styled.div`
+`;
+const Space = styled.div`
   height: 42px;
-`
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
@@ -43,10 +46,14 @@ function Tags() {
   return (
     <Layout>
       <TagList>
-        {tags.map(tag => <li key={tag}>
-          <span className='oneLine'>{tag}</span>
-          <Icon name="right"/>
-        </li>)}
+        {tags.map(tag =>
+          <li key={tag}>
+            <Link to={'/tags/' + tag}>
+              <span className='oneLine'>{tag}</span>
+              <Icon name="right"/>
+            </Link>
+          </li>
+        )}
       </TagList>
       <Center>
         <Space/>
